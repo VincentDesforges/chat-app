ef12f7db180ffccabd0927e01e01112a5409e2b1
Vincent Desforges <desforges.vincent@gmail.com>
2018-10-13 10:32:24 +0100

    add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -21,13 +21,39 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e) {
-   e.preventDefault();
+socket.on('newLocationMessage', function(message) {
+  const li = jQuery('<li></li>');
+  const a = jQuery('<a target="_blank">My current location</a>');
 
-   socket.emit('createMessage', {
-      from: 'User',
-      text: jQuery('[name=message]').val()
-    }, function(data) {
-      console.log('Got it!: ', data);
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(data) {
+    console.log('Got it!: ', data);
+  });
+});
+
+const locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
     });
+  }, function() {
+    alert('Unable to fetch location...');
+  });
 });
