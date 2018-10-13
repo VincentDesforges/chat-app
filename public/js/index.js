8db98117467997e274bd526f49cb3370497c9a8d
Vincent Desforges <desforges.vincent@gmail.com>
2018-10-13 13:19:52 +0100

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom () {
+  //Selectors
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+  //Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function() {
   console.log('Connected to server');
 
@@ -23,6 +39,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // console.log('New message: ', message);
   // const li = jQuery('<li></li>');
@@ -41,6 +58,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // const li = jQuery('<li></li>');
   // const a = jQuery('<a target="_blank">My current location</a>');
